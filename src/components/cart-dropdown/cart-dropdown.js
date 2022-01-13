d09d98444238a068c81f8bef4bcf24eec081f2cf
RssX <power_addict@yahoo.com>
2022-01-13 18:37:15 +0200

    final dropdown

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -1,12 +1,25 @@
 import CustomButton from '../custom-button/custom-button';
 import './cart-dropdown.scss';
 import React from 'react'
+import { connect } from 'react-redux';
+import CartItem from '../cart-item/cart-item';
 
-export default function CartDropdown() {
+const CartDropdown = ({cartItems}) => {
     return (
         <div className='cart-dropdown'>
-            <div className='cart-items'/>
+            <div className='cart-items'>
+                {
+                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                }
+            </div>
+
             <CustomButton>GO TO CHECKOUT</CustomButton>
         </div>
     )
 }
+
+const mapStateToProps = ({cart: {cartItems}}) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
